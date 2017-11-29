7f73732da8d353d86e613dca372f09802f8a32e6
richard <rmelos@unal.edu.co>
2017-11-28 20:01:54 -0500

    arreglando los valores de entrada de la pagina del conductor

---
+++ src/models/conductor-item/conductor-item.interface.ts
@@ -0,0 +1,9 @@
+export interface ConductorItem{
+  origen: String;
+  destino: String;
+  precio: String;
+  fecha: String;
+  hora: String;
+  cupos: number;
+  mesajeConductor: String;
+}
--- src/pages/conductor/conductor.ts
+++ src/pages/conductor/conductor.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ShoppingItem} from "../../models/shopping-item/shopping-item.interface";
 import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
+import {ConductorItem} from "../../models/conductor-item/conductor-item.interface";
 
 
 /**
@@ -22,11 +23,18 @@ export class ConductorPage {
 
   shoppingItemRef$: AngularFireList<any>
 
+
+  todo = {};
+  title = 'nacho';
+  coductorItem = {} as ConductorItem;
+  conductorItemRef$: AngularFireList<any>
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
               database: AngularFireDatabase) {
 
 
     this.shoppingItemRef$ = database.list('shopping-list');
+    this.conductorItemRef$ = database.list('conductor-lista');
   }
 
   ionViewDidLoad() {
@@ -50,6 +58,12 @@ export class ConductorPage {
     //console.log('############'+shoppingItem.itemName);
     console.log(shoppingItem);
     */
+  }
+
+
+  ruta() {
+    this.title = this.todo.toString();
+    console.log(this.todo)
   }
 
   navigateToPasajeroPage() {
